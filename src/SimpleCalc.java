9dd5f1c40a25a11ad9d08f5d3633ce023fbda3bc
Miniacz <briareus100@gmail.com>
2018-04-04 22:01:17 +0200

    testing menu items

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class SimpleCalc {
-    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // testowa klasa menu
+        class MenuItem {
+
+            private void displayElement(){
+                System.out.println("element called properly");
+            }
+        }
+
+        // testowa deklaracja i inicjalizacja instancji klas
+        MenuItem FirstOne = new MenuItem();
+        Scanner read = new Scanner(System.in);
+
+        // inicjalizacja inputu
+        int input_one = read.nextInt();
+
+        if (input_one > 0){
+            FirstOne.displayElement();
+        }
+
+    }
 }
