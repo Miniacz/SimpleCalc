89b87a9926620454e2dd9fb398e61f7dea98c0a0
Miniacz <briareus100@gmail.com>
2018-04-05 21:55:32 +0200

    working menu main loop

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -1,35 +1,50 @@
+// - dodanie więcej opcji menu
+// - dodać wyjątki - obsługa InputMismatchException
+// - opracować metody dla obługi poszczególnych opcji
+// - formatowanie całości (wodotryski i czyszczenie ekranu)
+
 import java.util.Scanner;
 
-public class SimpleCalc {
+class Main {
     public static void main(String[] args) {
-        System.out.println("\n");
+        System.out.println("Please pick menu option:" + "\n 1 - picks option (does nothing else)" + "\n 0 - quits" + "\n");
 
-        // testowa klasa menu
+        int menuControl = 1;
+
+        // klasa obsługująca poszczególne opcje
         class MenuItem {
 
-            private void displayElement(){
-                // test display - if works, display text
-                System.out.println("\n" + "element called properly");
+            void displayMenuItem(){
+                System.out.println("\n Menu option picked.\n");
             }
         }
 
-        // testowa deklaracja i inicjalizacja instancji klas
-        MenuItem FirstOne = new MenuItem();
-        Scanner read = new Scanner(System.in);
+        // klasa, która obługuje poszczególne metody matematyczne dla opcji kalkulatora
+        // class calcCompute {
 
-        int mainMenuBreak = 0;
+        //   int addXY(int x, int y){
 
-        while (mainMenuBreak == 1){
-                // inicjalizacja inputu
-                System.out.println("Please enter value:" + "\n");
-                int inputOne = read.nextInt();
+        //   }
+        // }
 
-                if (inputOne > 0) {
-                    FirstOne.displayElement();
-                } else {
-                    mainMenuBreak = 1;
-                }
+        MenuItem option1 = new MenuItem();
+
+        // główna pętla menu - ma powtarzać menu, dopóki użytkownik nie będzie chciał skończyć programu
+        while (menuControl > 0){
+            Scanner read = new Scanner(System.in);
+            menuControl = read.nextInt();
+
+            if (menuControl == 1){
+                option1.displayMenuItem();
+            } else if (menuControl > 1) {
+                System.out.println("\n ERROR - Wrong value picked." + "\nPlease enter value 1 or 0" + "\n");
             }
+
+            if (menuControl == 0){
+                System.out.println("\n" + "Bye!" + "\n");
+            }
+
+        }
 
     }
 }
