00b116a07437ae50aa7f2009dfedbc22ea55d4cd
Miniacz <briareus100@gmail.com>
2018-04-05 22:19:03 +0200

    working addition option in menu (values hardcoded)

--- src/SimpleCalc.java
+++ src/SimpleCalc.java
@@ -7,9 +7,23 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Please pick menu option:" + "\n 1 - picks option (does nothing else)" + "\n 0 - quits" + "\n");
+        System.out.println("Please pick menu option:" +
+                "\n 1 - picks option (does nothing else)" +
+                "\n 2 - adds two numbers (for now, it does 1+2=3)" +
+                "\n 0 - quits" + "\n");
 
         int menuControl = 1;
+
+        // klasa, która obługuje poszczególne metody matematyczne dla opcji kalkulatora
+//        class CalcCompute {
+//
+//            // metoda dla dodawania
+//            int addXY(int x, int y){
+//                int addXYsum;
+//                addXYsum = x + y;
+//                return addXYsum;
+//            }
+//        }
 
         // klasa obsługująca poszczególne opcje
         class MenuItem {
@@ -17,18 +31,12 @@ class Main {
             private void displayMenuItem(){
                 System.out.println("\n Menu option picked.\n");
             }
+            private int addXY(int x, int y){
+                int addXYsum;
+                addXYsum = x + y;
+                return addXYsum;
+            }
         }
-
-        // klasa, która obługuje poszczególne metody matematyczne dla opcji kalkulatora
-         class calcCompute {
-
-            // metoda dla dodawania
-           int addXY(int x, int y){
-               int addXYsum;
-               addXYsum = x + y;
-               return addXYsum;
-           }
-         }
 
         MenuItem option1 = new MenuItem();
 
@@ -39,8 +47,10 @@ class Main {
 
             if (menuControl == 1){
                 option1.displayMenuItem();
-            } else if (menuControl > 1) {
+            } else if (menuControl > 2) {
                 System.out.println("\n ERROR - Wrong value picked." + "\nPlease enter value 1 or 0" + "\n");
+            } else if (menuControl == 2) {
+                System.out.println("\n Addition result: " + option1.addXY(1,2) + "\n");
             }
 
             if (menuControl == 0){
