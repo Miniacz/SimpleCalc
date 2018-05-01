be39185422b6e58fd7e2cbf4e88a22623743fe1c
Miniacz <briareus100@gmail.com>
2018-05-01 17:58:46 +0200

    code cleanup in InputValueContainer class

--- src/calcEngine/InputValueContainer.java
+++ src/calcEngine/InputValueContainer.java
@@ -8,8 +8,6 @@ public class InputValueContainer {
     private InputValidator checkedInput = new InputValidator();
     private double x;
     private double y;
-    private double memory = 0;
-
 
     /*
      * Input handling section
@@ -37,42 +35,5 @@ public class InputValueContainer {
     private void setY() {
         this.y = checkedInput.validateInput();
     }
-
-    /*
-    * Memory handling section
-    * */
-
-    // Pobiera jako argument wartość, która ma być wprowadzona do pamięci podręcznej kalkulatora
-    public void memoryQuery(double memoryInput) {
-        boolean queryOnOff = true;
-
-        System.out.println(
-                "\n Do You want to save the result?" +
-                        "\n Please use numbers 1 or 2." +
-                        "\n 1: Yes." +
-                        "\n 2: No." + "\n");
-
-        while (queryOnOff) {
-            this.x = checkedInput.validateInput();
-            if (x == 1) {
-                this.memory = memoryInput;
-                // System.out.println("\n Memory value changed to: " + this.getMemory());
-                queryOnOff = false;
-            } else if (x == 2) {
-                // System.out.println("\n Memory value remained unchanged. Current value is: " + this.getMemory());
-                queryOnOff = false;
-            } else {
-                System.out.println("\n You have entered an invalid number. Please use 1 or 2. \n");
-            }
-        }
-
-    }
-
-    public double getMemory() {
-        return memory;
-    }
-
-    private void setMemory(double memory) {
-        this.memory = memory;
-    }
+    
 }
